ffada7b85e57232dffb5231ebb7c9b06f181352e
Samuel Kowatschek <samkow@gmx.at>
2019-04-11 11:43:07 +0200

    started

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -10,3 +10,28 @@
  * Implementation of a library for analyzing lottery tips.
  *-----------------------------------------------------------------------------
 */
+#include "lottery.h"
+#include <stdio.h>
+
+bool init_lottery(const char *csv_file, char csv_separator){
+    FILE* stream = fopen(csv_file, "r");
+    if(stream == 0) return false;
+    char* lines = fgets(csv_seperator, MAX_LINE_LEN, stream);
+    return true;
+}
+ 
+bool get_tip(int tip_number, int tip[TIP_SIZE]){
+    return true;
+}
+ 
+bool set_drawing(int drawing_numbers[TIP_SIZE]){
+    return 0;
+}
+ 
+int get_tip_result(int tip_number){
+    return 0;
+}
+ 
+int get_right_tips_count(int right_digits_count){
+    return 0;
+}
--- assignment/lottery.h
+++ assignment/lottery.h
@@ -11,6 +11,22 @@
  * ----------------------------------------------------------
  */
 #ifndef ___LOTTERY_H
-#define ___LOTTERY_H
+#define ___LOTTERY_H 
+#include "general.h"
+#define TIP_SIZE 6
+#define UUID_LEN 40
+#define MAX_TIP_LEN 17
+#define MAX_LINE_LEN (UUID_LEN + 1 + MAX_TIP_LEN + 1)
 
+ 
+bool init_lottery(const char *csv_file, char csv_separator);
+ 
+bool get_tip(int tip_number, int tip[TIP_SIZE]);
+ 
+bool set_drawing(int drawing_numbers[TIP_SIZE]);
+ 
+int get_tip_result(int tip_number);
+ 
+int get_right_tips_count(int right_digits_count);
+ 
 #endif

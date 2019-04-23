5ad0ce95e7b52d0691253ee22382fbee172153ec
samuel-kowatschek <samkow@gmx.at>
2019-04-23 19:02:33 +0200

    added a functions contains() started the get_tip() function again

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 char seperator;
 FILE* stream;
@@ -36,9 +37,45 @@ bool is_tip_number_valid(int tip_number){
                                                                 // we get the actual position we would get with this tip_number 
 }
 
+bool contains(int number, int numbers[TIP_SIZE])
+{
+    for (int i = 0; i < TIP_SIZE; i++)
+    {
+        if (numbers[i] == number)
+            return true;
+    }
+    return false;
+}
+
+
 bool get_tip(int tip_number, int tip[TIP_SIZE]){
-    bool t = is_tip_number_valid(tip_number);
-    return t;
+    if(is_tip_number_valid(tip_number)){
+            fseek(stream, MAX_LINE_LEN * tip_number, SEEK_SET);
+            fseek(stream, UUID_LEN, SEEK_CUR);
+            
+            char tip_string[MAX_TIP_LEN];
+            fgets(tip_string, MAX_TIP_LEN, stream);
+
+            char *tkn; 
+            char sep[] = { seperator };
+            tkn = strtok(tip_string, sep);
+
+            int pos = 0;
+            while(tkn != 0 && pos < TIP_SIZE) {
+                if (!contains(atoi(tkn), tip))
+                    return false;
+        
+            if(tkn[1] != 0){
+                tip[pos] = (tkn[0] - '0') + (tkn[1] - '0');
+            }else{
+            tip[pos] = tkn[0] - '0';
+            }
+            tkn = strtok(0, sep);
+            pos++;
+        }
+        return true;
+    }
+    return false;
 }
  
 bool set_drawing(int drawing_numbers[TIP_SIZE]){

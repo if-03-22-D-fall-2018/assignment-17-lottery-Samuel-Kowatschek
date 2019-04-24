3171d776c4999e990c53b893405bbe2f2daa9c6b
samuel-kowatschek <samkow@gmx.at>
2019-04-24 18:23:09 +0200

    changed most of the program deleted functions added new functions changed exisiting functions and globals

--- assignment/lottery.cpp
+++ assignment/lottery.cpp
@@ -1,98 +1,101 @@
-/*-----------------------------------------------------------------------------
- *				HTBLA-Leonding / Class: <your class name here>
- *-----------------------------------------------------------------------------
- * Exercise Number: #exercise_number#
- * File:			lottery.c
- * Author(s):		Peter Bauer
- * Due Date:		#due#
- *-----------------------------------------------------------------------------
- * Description:
- * Implementation of a library for analyzing lottery tips.
- *-----------------------------------------------------------------------------
-*/
-#include "lottery.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include "lottery.h"
 
-char seperator;
-FILE* stream;
+#define 	UUID_LEN       36
+#define 	MAX_TIP_LEN    17
 
-bool init_lottery(const char *csv_file, char csv_separator){
-    stream = fopen(csv_file, "r");
-    seperator = csv_separator;
-    return stream != 0;
-}
+//Unused?
+#define 	MAX_LINE_LEN   (UUID_LEN + 1 + MAX_TIP_LEN)
 
-bool is_tip_number_valid(int tip_number){
-    //find out file size
-    int prev=ftell(stream);         ///previous position
-    fseek(stream, 0L, SEEK_END);    ///End of file
-    int sz=ftell(stream);           ///sz = size, tells me the position of the stream
-    fseek(stream,prev,SEEK_SET); //go back to where we were
-    
-    return tip_number >= 0 && tip_number * MAX_LINE_LEN <= sz; //tip_number is for example 30,
-                                                                // if we multiply it by the MAX_LINE_LEN,
-                                                                // we get the actual position we would get with this tip_number 
-}
+int get_file_length(FILE* fd);
+int get_line_length(FILE* fd, int line_index);
 
-bool contains(int number, int numbers[TIP_SIZE])
+FILE* fd;
+char separator;
+
+bool init_lottery(const char *csv_file, char csv_separator)
 {
-    for (int i = 0; i < TIP_SIZE; i++)
-    {
-        if (numbers[i] == number)
-            return true;
-    }
-    return false;
+    fd = fopen(csv_file, "r");
+    separator = csv_separator;
+    return fd != 0;
 }
 
+bool get_tip(int tip_number, int tip[TIP_SIZE])
+{
+    //Validate tip number
+    if (tip_number < 0 || tip_number >= 1000000)
+        return false;
 
-bool get_tip(int tip_number, int tip[TIP_SIZE]){
-    if(is_tip_number_valid(tip_number)){
-            fseek(stream, MAX_LINE_LEN * tip_number, SEEK_SET);
-            fseek(stream, UUID_LEN, SEEK_CUR);
-            
-            char temp[MAX_TIP_LEN + 1];
-            fgets(temp, MAX_TIP_LEN, stream);
-            char tip_string[MAX_TIP_LEN];
-            for(int i = 1; i <= MAX_TIP_LEN; i++)
-            {
-                tip_string[i-1] = temp[i];
-            }
-            
-
-
-            char *tkn; 
-            char sep[] = { seperator };
-            tkn = strtok(tip_string, sep);
-
-            int pos = 0;
-            while(tkn != 0 && pos < TIP_SIZE) {
-                if (!contains(atoi(tkn), tip))
-                    return false;
-        
-            if(tkn[1] != 0){
-                tip[pos] = (tkn[0] - '0') + (tkn[1] - '0');
-            }else{
-            tip[pos] = tkn[0] - '0';
-            }
-            tkn = strtok(0, sep);
-            pos++;
+    //Jump to tip
+    //Iterate to target line start (can't use fseek() because each line is differently long)
+    rewind(fd);
+    int currentLine = 0; //Representing the current line in file
+    while (currentLine < tip_number){
+        while(fgetc(fd) == '\n'){
+           currentLine++;
+           break;
         }
-        return true;
     }
-    return false;
+
+    //Skip UUID (+first separator)
+    fseek(fd, UUID_LEN + 1, SEEK_CUR);
+
+    //Read tip string
+    char tipString[MAX_TIP_LEN];
+    fgets(tipString, MAX_TIP_LEN, fd);
+
+    //Split tip string
+    char delimiter[2] = { separator, '\0' };
+    char *ptr = strtok(tipString, delimiter);
+
+    int pos = 0;
+    while(ptr != NULL) {
+        int tipNumber = atoi(ptr);
+        tip[pos] = tipNumber;
+        ptr = strtok(0L, delimiter);
+        pos++;
+    }
+    return true;
 }
- 
-bool set_drawing(int drawing_numbers[TIP_SIZE]){
+
+bool set_drawing(int drawing_numbers[TIP_SIZE])
+{
+  return 0;
+}
+
+int get_tip_result(int tip_number)
+{
     return 0;
 }
- 
-int get_tip_result(int tip_number){
+
+int get_right_tips_count(int right_digits_count)
+{
     return 0;
 }
- 
-int get_right_tips_count(int right_digits_count){
-    return 0;
+
+int get_file_length(FILE* fd)
+{
+    int previousPos = ftell(fd);
+    fseek(fd, 0, SEEK_END);
+    int end = ftell(fd);
+    fseek(fd, previousPos, SEEK_SET);
+    return end;
+}
+
+int get_line_length(FILE* fd, int line_index)
+{
+    int previousPos = ftell(fd);    //Backup previous pos
+    int length = 0;
+    rewind(fd);        //Jump to start of file
+
+    //We've reached the point where we are before the line we want to count.
+    //Now determine length of line
+    while (!feof(fd) && fgetc(fd) != '\n')
+        length++;
+
+    //Jump to previous pos
+    fseek(fd, previousPos, SEEK_SET);
+    return length;
 }
